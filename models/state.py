4d05d2e9eb1459064cf251bc5297bd7d767eb683
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-19 11:13:57 -0500

    add getter cities to state model

--- models/state.py
+++ models/state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+import models
 import os
+
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -17,3 +20,15 @@ class State(BaseModel, Base):
         cities = relationship("City", cascade="all", backref="state")
     else:
         name = ""
+
+        @property
+        def cities(self):
+            '''
+            getter attribute cities
+            '''
+            city_list = []
+            cities = models.storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
