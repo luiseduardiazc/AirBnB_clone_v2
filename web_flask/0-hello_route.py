db59248a7ecf457728418ed99e2818fd4256fe74
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-20 17:45:16 -0500

    add docstring

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
-import os
+'''
+module hello
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
+    ''' route / '''
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
