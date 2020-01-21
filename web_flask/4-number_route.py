d8e32a98acfad5306985e94b32f37b1baa77aa8f
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-21 13:19:58 -0500

    add more task to project

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+'''
+module hello
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' route / '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' route /hbnb '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    '''
+    Recive text by parameter
+    '''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is(text):
+    '''
+    Recive optional text
+    The default value of text is "is cool"
+    '''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_a_number(n):
+    '''
+    is a number
+    '''
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+'''
+module hello
+'''
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' route / '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' route /hbnb '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    '''
+    Recive text by parameter
+    '''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is(text):
+    '''
+    Recive optional text
+    The default value of text is "is cool"
+    '''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_a_number(n):
+    '''
+    is a number
+    '''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def is_a_number_template(n):
+    '''
+    is a number template
+    '''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
