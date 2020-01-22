5f11c1886ba890d9e0a13a401ee481eaca4ab40b
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-22 00:43:55 -0500

    add hbnb_filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+module hbnb_filters
+'''
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def find_state():
+    """ hbnb_filters
+    """
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    ''' teardown_db '''
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
